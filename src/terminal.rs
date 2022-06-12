25705693c684edc24494874b80d8beb1b9fb3901
Jordi Domenech Bonilla <jdomenechbborges@gmail.com>
2022-06-12 15:24:09 +0200

    Fixed problem with colors

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,17 +1,25 @@
 use colored::Colorize;
 
 pub fn format_error(message: &str) -> String {
-    format!("ERROR: {}", message.red())
+    format!("ERROR: {}", message).red().to_string()
 }
 
-pub fn print_warning(message: String) {
-    println!("WARNING: {}", message.yellow());
+pub fn format_warning(message: &str) -> String {
+    format!("WARNING: {}", message).yellow().to_string()
+}
+
+pub fn format_success(message: &str) -> String {
+    format!("{}", message).green().to_string()
 }
 
 pub fn print_error(message: String) {
-    println!("ERROR: {}", message.red());
+    println!("{}", format_error(message.as_str()));
+}
+
+pub fn print_warning(message: String) {
+    println!("{}", format_warning(message.as_str()));
 }
 
 pub fn print_success(message: String) {
-    println!("{}", message.green());
+    println!("{}", format_success(message.as_str()));
 }
